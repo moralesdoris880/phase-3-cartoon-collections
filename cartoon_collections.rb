d3738ab55a4e5cae26de0bdee815d1235c5a5967
Doris Morales <moralesdoris880@gmail.com>
2022-02-03 19:06:18 -0500

    commit tree 1fb8dd991008527abcf2c953eef833a827480401

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index{|name,index| puts "#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  newplaneteer_call= planeteer_calls.map{|name| "#{name.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  if words.find{|word|word.length>4}
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if foods.include?(cheese_types[0])
+    puts foods[foods.index(cheese_types[0])]
+  elsif foods.include?(cheese_types[1])
+    puts foods[foods.index(cheese_types[1])]
+  elsif foods.include?(cheese_types[2])
+      puts foods[foods.index(cheese_types[2])]
+  else
+    nil
+  end
 end
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
